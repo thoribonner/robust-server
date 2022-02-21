8584c2519c598124141d223a327aa1338cf1c9b0
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-02-21 13:20:09 -0500

    restful apis complete

--- src/app.js
+++ src/app.js
@@ -4,14 +4,43 @@ const pastes = require("./data/pastes-data");
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
+app.use(express.json());
 // * routes
-app.use('/pastes/:pasteId', (req, res, next) => {
+app.use("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
-  const foundPaste = pastes.find( paste => paste.id === Number(pasteId));
-  foundPaste ? res.json({ data: foundPaste }) : next(`Paste id not found: ${pasteId}`);
-})
-app.use("/pastes", (req, res) => {
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+  foundPaste
+    ? res.json({ data: foundPaste })
+    : next(`Paste id not found: ${pasteId}`);
+});
+
+// * api requests
+app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
+});
+
+app.post("/pastes", (req, res, nxt) => {
+  let lastPasteId = pastes.reduce(
+    (maxId, paste) => Math.max(maxId, paste.id),
+    0
+  );
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
+    req.body;
+  if (text) {
+    const newPaste = {
+      id: ++lastPasteId,
+      name,
+      syntax,
+      exposure,
+      expiration,
+      text,
+      user_id,
+    };
+    pastes.push(newPaste);
+    res.status(201).json({ data: newPaste });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 // * not found handler
