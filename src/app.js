55ebf2c84a2ee4bdc38479520ea2b88248e9b1cb
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-02-21 11:25:46 -0500

    static data complete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const app = express();
+const pastes = require("./data/pastes-data");
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
-// Not found handler
-app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+// * routes
+app.use('/pastes/:pasteId', (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find( paste => paste.id === Number(pasteId));
+  foundPaste ? res.json({ data: foundPaste }) : next(`Paste id not found: ${pasteId}`);
+})
+app.use("/pastes", (req, res) => {
+  res.json({ data: pastes });
 });
 
-// Error handler
-app.use((error, request, response, next) => {
-  console.error(error);
-  response.send(error);
+// * not found handler
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
+});
+
+// * error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send(err);
 });
 
 module.exports = app;
