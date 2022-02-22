125802a93fbc4084f06011a2eef8f279e2b54d24
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-02-21 19:44:30 -0500

    major error types and handling complete

--- src/app.js
+++ src/app.js
@@ -2,56 +2,55 @@ const express = require("express");
 const app = express();
 const pastes = require("./data/pastes-data");
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
-
+// * middleware
 app.use(express.json());
+
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+function bodyHasTextProperty(req, res, nxt) {
+  const { data: { text } = {} } = req.body;
+  text ? nxt() : nxt({ status: 400, message: `A 'text' property is required.`});
+}
+
 // * routes
-app.use("/pastes/:pasteId", (req, res, next) => {
+app.use("/pastes/:pasteId", (req, res, nxt) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
   foundPaste
     ? res.json({ data: foundPaste })
-    : next(`Paste id not found: ${pasteId}`);
+    : nxt({ status: 404, message: `Paste id not found: ${pasteId}` });
 });
 
-// * api requests
 app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
 });
 
-app.post("/pastes", (req, res, nxt) => {
-  let lastPasteId = pastes.reduce(
-    (maxId, paste) => Math.max(maxId, paste.id),
-    0
-  );
+app.post("/pastes", bodyHasTextProperty, (req, res, nxt) => {
   const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
     req.body;
-  if (text) {
-    const newPaste = {
-      id: ++lastPasteId,
-      name,
-      syntax,
-      exposure,
-      expiration,
-      text,
-      user_id,
-    };
-    pastes.push(newPaste);
-    res.status(201).json({ data: newPaste });
-  } else {
-    res.sendStatus(400);
-  }
+  const newPaste = {
+    id: ++lastPasteId,
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+  pastes.push(newPaste);
+  res.status(201).json({ data: newPaste });
 });
 
 // * not found handler
-app.use((req, res, next) => {
-  next(`Not found: ${req.originalUrl}`);
+app.use((req, res, nxt) => {
+  nxt({status: 404, message: `Not found: ${req.originalUrl}` });
 });
 
 // * error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res, nxt) => {
   console.error(err);
-  res.send(err);
+  const { status = 500, message = "Something went wrong!" } = err
+  res.status(status).json({error : message});
 });
 
 module.exports = app;
